8bcacf4678fd426780f098ac3639afa82553b378
Graham Spencer <gspencer447@yahoo.com>
2023-06-09 13:52:03 -0500

    finished implementing lifecycleMethods exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,6 @@ import './App.css';
 import React from 'react';
 import FilmsList from './components/FilmsList';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>This is my h1</h1>
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           React is so cool!
-//         </p>
-//         <p>This is my new paragraph!</p>
-//         <ul>
-//           <li>List item 1</li>
-//           <li>List item 2</li>
-//           <li>List item 3</li>
-//         </ul>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends React.Component {
   constructor() {
@@ -49,7 +22,7 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className='App'>
         <h1>Hello World</h1>
         <form action="" onSubmit ={this.onSubmit}>
           <input type="text" value = {this.state.text}
